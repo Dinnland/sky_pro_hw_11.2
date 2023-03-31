58ca1bb0424841fa49371e6be00ad2d72a6dda94
dinar mingazov <dinarek2013@gmail.com>
2023-03-31 14:57:40 +0300

    add: 1)def capital_tittle(line) 2) docstrings for:def capital_tittle(line), def capital_letters(line)

--- str_func.py
+++ str_func.py
@@ -1,4 +1,16 @@
 def capital_letters(line):
+    """
+    делает буквы в тексте заглавными
+    """
     return line.upper()
 
-#print(capital_letters('апваопвап'))
+# print(capital_letters('апваопвап'))
+
+
+def capital_tittle(line):
+    """
+    делает заглавными первые буквы каждого слова
+    """
+    return line.title()
+
+# print(capital_tittle('апваопвап hgh jdfhgsd dfgsd'))
